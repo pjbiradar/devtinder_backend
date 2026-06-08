aad2743c86781c40c2cb2e63397f75fd47801154
pooja biradar <poojabiradar2412@gmail.com>
2026-06-08 14:56:52 +0530

    Implement multiple route handlers with next()

--- src/app.js
+++ src/app.js
@@ -3,12 +3,32 @@ const express = require("express")
 const app = express();
 
 
+//router handlers
+app.use("/user",(req,res,next)=>{
+    console.log("this is response1")
+    // res.send("this is response1")
+    next()
+},
+(req,res,next)=>{
+    res.send("this is response2")
+    next();
+},
+
+(req,res,next)=>
+{
+    res.send("this is response 3")
+    next();
+
+}
+)
+
+
 //this is dynamic routes 
-app.get('/user/:id/:firstname/:lastname',(req,res)=>{
-    // console.log(req.query)
-    console.log(req.params)
-    res.send({firstname: "pooja", lastname: "biradar"})
-})
+// app.get('/user/:id/:firstname/:lastname',(req,res)=>{
+//     // console.log(req.query)
+//     console.log(req.params)
+//     res.send({firstname: "pooja", lastname: "biradar"})
+// })
 
 app.post('/user',(req,res)=>{
     res.send("This is the post http request for user")
@@ -21,6 +41,8 @@ app.patch('/user',(req,res)=>{
 app.delete('/user',(req,res)=>{
     res.send("This is the delete http request for user")
 })
+
+
 
 app.listen(3000,()=>{
     console.log("server running on port 3000")
