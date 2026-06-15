11635709c69089a8189b76cf4898d099a7affcab
pooja biradar <poojabiradar2412@gmail.com>
2026-06-15 18:16:12 +0530

    error handling in middlewares

--- src/app.js
+++ src/app.js
@@ -2,29 +2,52 @@ const express = require("express")
 
 const app = express();
 
-const {adminAuth,userAuth} = require('../middleware/auth')
 
+//errorhandling
+// app.get('/user',(err,req,res,next)=>{
+//     try{
+//         //logic of db call and getuserdata
+       
+//         throw new Error("DB failed");
+//         res.send("userdatasent");
+//     }
+//     catch(err){
+//         //log your error
+//        res.status(500).send("something went wrong")
+      
+//     }
+// })
+
+// app.use((err, req, res, next) => {
+//     console.error(err.message); // log error
+  
+//     res.status(500).json({
+//       message: "Something went wrong"
+//     });
+//   });
+
+
+
+/////////////////////////////////////
+//middleware 
+// const {adminAuth,userAuth} = require('../middleware/auth')
 //for userauth
 // app.use("/user",userAuth)
-app.get("/user/adduser",userAuth,(req,res,next)=>{
-    res.send("useris added")
-})
-
-
-app.use("/admin",adminAuth)
-
-app.get("/admin/alldata",(req,res,next)=>{
-    res.send("all data reviewed")
-})
-
-app.post("/admin/deletedata",(req,res,next)=>{
-    res.send("all data deleted")
-})
+// app.get("/user/adduser",userAuth,(req,res,next)=>{
+//     res.send("useris added")
+// })
+// app.use("/admin",adminAuth)
+// app.get("/admin/alldata",(req,res,next)=>{
+//     res.send("all data reviewed")
+// })
+// app.post("/admin/deletedata",(req,res,next)=>{
+//     res.send("all data deleted")
+// })
+///////////////////////////////////////////////////
 
 
 
-
-
+////////////////////////////////////////////////////////
 //this is dynamic routes 
 // app.get('/user/:id/:firstname/:lastname',(req,res)=>{
 //     // console.log(req.query)
@@ -32,18 +55,19 @@ app.post("/admin/deletedata",(req,res,next)=>{
 //     res.send({firstname: "pooja", lastname: "biradar"})
 // })
 
-app.post('/user',(req,res)=>{
-    res.send("This is the post http request for user")
-})
+// app.post('/user',(req,res)=>{
+//     res.send("This is the post http request for user")
+// })
 
-app.patch('/user',(req,res)=>{
-    res.send("This is the patch http request for user")
-})
+// app.patch('/user',(req,res)=>{
+//     res.send("This is the patch http request for user")
+// })
 
-app.delete('/user',(req,res)=>{
-    res.send("This is the delete http request for user")
-})
+// app.delete('/user',(req,res)=>{
+//     res.send("This is the delete http request for user")
+// })
 
+///////////////////////////////////////////////
 
 
 app.listen(3000,()=>{
